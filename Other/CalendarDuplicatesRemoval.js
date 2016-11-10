d406c5ed7c942280cae5144eee1d208d249af518
kfbow <kolbe@sellerlabs.com>
2016-11-10 15:47:45 -0500

    Added notes

--- Other/CalendarDuplicatesRemoval.js
+++ Other/CalendarDuplicatesRemoval.js
@@ -3,16 +3,20 @@ function removeCalDup() {
     day = [];
     date = [];
 
+    // months of the year
     for (i=1;i<13;i++) {
         month.push(i);
 
+        // days of the month assuming 31 every month
         for (d=1;d<32;d++) {
             day.push(d);
 
+            // combine [mmdd]
             date.push(
                 ('00'+i).substring(i.toString().length)
-                + '' + ('00'+d).substring(d.toString().length));
+                + ('00'+d).substring(d.toString().length));
 
+            // for fun: remove every duplicate integer from each set of [mmdd]
             for (k=0;k<date.length;k++) {
                 set = date[k].split('')
                 end = set.filter(function(item, pos, self){
